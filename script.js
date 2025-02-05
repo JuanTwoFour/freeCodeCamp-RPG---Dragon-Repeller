8d6b89b4d782c3f24bf28aee3e0261f41ae0ea9c
Juan Vega <juanjvega13@gmail.com>
2025-02-04 19:26:43 -0600

    add lose function to locations

--- script.js
+++ script.js
@@ -89,6 +89,12 @@ const locations = [
     "button functions": [goTown, goTown, goTown],
     text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold',
   },
+  {
+    name: "lose",
+    "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+    "button function": [restart, restart, restart],
+    text: "You die. &#x2620;",
+  },
 ];
 
 function update(location) {
